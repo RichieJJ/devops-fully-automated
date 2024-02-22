972f9fa2896cd139516e3ab5863efb6ff06684f4
RichieJJ <112990571+RichieJJ@users.noreply.github.com>
2024-02-22 03:06:17 -0500

    Update jenkins-maven-ansible-setup.sh

--- jenkins-maven-ansible-setup.sh
+++ jenkins-maven-ansible-setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Hardware requirements: AWS Linux 2 with mimum t2.medium type instance & port 8080(jenkins), 9100 (node-exporter) should be allowed on the security groups
 # Installing Jenkins
-sudo yum update –y
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade
-sudo amazon-linux-extras install java-openjdk11 -y
-sudo yum install jenkins -y
-sudo echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
+# sudo yum update –y
+# sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+# sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+# sudo yum upgrade
+# sudo amazon-linux-extras install java-openjdk11 -y
+# sudo yum install jenkins -y
+# sudo echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+# sudo systemctl enable jenkins
+# sudo systemctl start jenkins
 
 sudo yum update –y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
