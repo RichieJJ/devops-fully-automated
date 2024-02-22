9bccdff3cb62998d22c806ed56b08cf63b6bc399
RichieJJ <112990571+RichieJJ@users.noreply.github.com>
2024-02-22 00:52:39 -0500

    Update jenkins-maven-ansible-setup.sh
    updated the jenkins user data

--- jenkins-maven-ansible-setup.sh
+++ jenkins-maven-ansible-setup.sh
@@ -11,6 +11,20 @@ sudo echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+# Add required dependencies for the jenkins package
+# sudo yum install fontconfig java-17-openjdk -y
+sudo amazon-linux-extras install java-17-openjdk -y
+sudo yum install jenkins -y
+# sudo systemctl daemon-reload
+sudo echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
 # Installing Git
 sudo yum install git -y
 
